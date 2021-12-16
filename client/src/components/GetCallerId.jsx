e633f2f48baf306962f61ae1071d38a4fdaab08e
Johanna Gerdts <jg@mavenlist.eu>
2021-12-17 00:16:34 +0100

    fixed callerId mess

---
+++ client/src/components/GetCallerId.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { SocketContext } from '../context/socket';
+import axios from 'axios'
+import { AuthContext } from '../context/auth'
+
+import { useParams, Link } from 'react-router-dom'
+
+
+export default function GetCallerId(props){
+    const { user } = useContext(AuthContext);
+    const storedToken = localStorage.getItem('authToken');
+    const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
+    // const [idToCall, setIdToCall] = useState('');
+    // const classes = useStyles();
+    const [username, setUsername] = useState('')
+    const [socketId, setSocketId] = useState('')
+
+    const { id } = useParams()
+
+    const getId = (e) => {
+        e.preventDefault();
+        // send a post request with the data from the state to the server
+        //to create new friend
+        const requestBody = { username: username, user: user };
+        //console.log('the body', requestBody);
+           axios.get(`/api/userprofile/${id}`, {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    }
+    )
+  .then(response => {
+      console.log('response is the following',response.data)
+      const {socketId} = response.data
+      // setUsername(username)
+      // setFriends(friends)
+      setSocketId(socketId)
+      console.log('calling socker ID number', socketId)
+  })
+  .catch(err => console.log(err))
+      };
+      return (
+        <div>
+            <h1>Get Caller ID</h1>
+            <form onSubmit={getId}>
+                <label className="spacing" htmlFor="username">Username: </label> 
+               <input className="spacing input-border" id="name" type="text" placeholder="Type friend's username..." value={username} onChange={e => setUsername(e.target.value)}/>
+                <br></br>
+               <button type="submit">Add this Friend</button>
+           </form>
+        </div>
+      )
+};
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useContext } from 'react';
 import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios'
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
 import { SocketContext } from '../context/socket';
 import { AuthContext } from '../context/auth'
 
+// export default function Sidebar() {
+//   const { user } = useContext(AuthContext);
+//   const storedToken = localStorage.getItem('authToken');
+//   const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
+//   // const [idToCall, setIdToCall] = useState('');
+//   // const classes = useStyles();
+//   const [username, setUsername] = useState('')
+//   const [friends, setFriends] = useState('')
+//   const [socketId, setSocketId] = useState('')
+//   const [idToCall, setIdToCall] = useState('');
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,6 +49,29 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// const getCallerId = (e) => {
+//   e.preventDefault();
+//   // send a post request with the data from the state to the server
+//   //to create new friend
+//   const requestBody = { username: username };
+//   console.log('the body', requestBody);
+//     axios.get(`/api/userprofile/${username}`
+//     , {
+//       headers: { Authorization: `Bearer ${storedToken}` },
+//     }
+//     )
+//   .then(response => {
+//       console.log('response is the following',response.data)
+//       const {socketId} = response.data
+//       // setUsername(username)
+//       // setFriends(friends)
+//       setSocketId(socketId)
+//       console.log('calling socker ID number', socketId)
+//       //callUser(socketId)
+//   })
+//   .catch(err => console.log(err))
+// };
+
 // function callUsername (userToCall){
   //get user
 
@@ -62,25 +95,24 @@ const useStyles = makeStyles((theme) => ({
 // }
 
 const Sidebar = ({ children }) => {
-  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
-  const [idToCall, setIdToCall] = useState('');
-  const classes = useStyles();
+const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
+ const [idToCall, setIdToCall] = useState('');
+ const classes = useStyles();
 
   return (
     <Container className={classes.container}>
       <Paper elevation={10} className={classes.paper}>
+      {/* <h1>Get Caller ID</h1> */}
+            {/* <form onSubmit={getCallerId}>
+                <label className="spacing" htmlFor="username">Username: </label>
+               <input className="spacing input-border" id="name" type="text" placeholder="Type friend's username..." value={username} onChange={e => setUsername(e.target.value)}/>
+                <br></br>
+               <button type="submit">Get Caller ID</button>
+           </form>
+           ID to call is: {socketId} (Copy to call) */}
         <form className={classes.root} noValidate autoComplete="off">
           <Grid container className={classes.gridContainer}>
             <Grid item xs={12} md={6} className={classes.padding}>
-
-
-            <h1>Get Caller ID</h1>
-            <form onSubmit={getCallerId}>
-               {/* <label className="spacing" htmlFor="username">Username: </label> */}
-               <input className="spacing input-border" id="name" type="text" placeholder="Type friend's username..." value={username} onChange={e => setUsername(e.target.value)}/>
-                <br></br>
-               <button type="submit">Add this Friend</button>
-           </form> 
 
             
               <Typography gutterBottom variant="h6">Make a call</Typography>
